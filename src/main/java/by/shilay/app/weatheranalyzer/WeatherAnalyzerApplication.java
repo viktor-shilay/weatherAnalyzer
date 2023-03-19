7758f382f3be1a1e121c45fbb31e2f938913198f
Viktor Shilay <viktor.shilay@gmail.com>
2023-03-19 12:32:59 +0300

    init project

---
+++ src/main/java/by/shilay/app/weatheranalyzer/WeatherAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package by.shilay.app.weatheranalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherAnalyzerApplication.class, args);
+    }
+
+}
